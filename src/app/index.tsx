5f98935837c9142342fe12cef2529417b996460e
matheus98rocha <matheus98rocha@gmail.com>
2024-02-06 21:01:47 -0300

    Finalizando a primeira aula

---
+++ src/app/index.tsx
@@ -0,0 +1,33 @@
+import CategoryButton from "@/components/category-button";
+import { CATEGORIES } from "@/utils/data/products";
+import Header from "@/components/header";
+import { View, FlatList } from "react-native";
+import { useCallback, useState } from "react";
+
+export default function Home() {
+  const [category, setCategory] = useState<string>(CATEGORIES[0]);
+
+  const handleCategorySelect = useCallback((category: string) => {
+    setCategory(category);
+  }, []);
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Faça o seu pedido" cartQuantityItems={1} />
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            onPress={() => handleCategorySelect(item)}
+            isSelected={category === item}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+    </View>
+  );
+}
---
+++ src/components/category-button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Pressable, Text, PressableProps } from "react-native";
+import { clsx } from "clsx";
+
+interface CategoryProps extends PressableProps {
+  title: string;
+  isSelected?: boolean;
+}
+
+function CategoryButton({ title, isSelected, ...rest }: CategoryProps) {
+  return (
+    <Pressable
+      className={clsx(
+        "bg-slate-800 px-4 justify-center rounded-md h-10",
+        isSelected && "border-2 border-lime-300"
+      )}
+      {...rest}
+    >
+      <Text className="text-slate-100 font-subtitle text-sm">{title}</Text>
+    </Pressable>
+  );
+}
+
+export default CategoryButton;
